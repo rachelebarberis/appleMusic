756bc7e7a6f7df686541f80378779eb9dc63a7ad
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-24 11:43:14 +0100

    commit

--- src/Components/SectionAltro.jsx
+++ src/Components/SectionAltro.jsx
@@ -6,109 +6,163 @@ const SectionAltro = () => {
       <p className="text-white fw-bold">Altro da Esplorare</p>
       <Row>
         <Col md={4}>
-          <div className="bg-dark pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2"
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2"
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right "
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2 ">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2"
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
         </Col>
         <Col md={4}>
-          <div className="bg-dark  pt-3 ps-2 m-1 rounded-2">
+          <div
+            className="pt-3 ps-2 m-1 rounded-2 "
+            style={{ backgroundColor: "#2A2A2A" }}
+          >
             <Row className="justify-content-between">
               <Col xs={10}>
-                <p className="text-danger">Esplora per genere</p>
+                <p style={{ color: "#EB5465" }}>Esplora per genere</p>
               </Col>
               <Col xs={2}>
-                <i className="bi bi-chevron-right text-danger"></i>
+                <i
+                  className="bi bi-chevron-right"
+                  style={{ color: "#EB5465" }}
+                ></i>
               </Col>
             </Row>
           </div>
